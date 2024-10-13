61e462211eff4845f62d6a576240c94a46ec5e81
Baran Ausmus <ausmusbaran@gmail.com>
2024-10-12 23:30:26 -0500

    fourth commit

--- script1.py
+++ script1.py
@@ -4,15 +4,15 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('AusmusBaranHomePage.html')
+    return render_template("home.html")
 
-@app.route('/SoftwareInterests/')
+@app.route('/interests/')
 def interests():
-    return render_template('SoftwareInterests.html')
+    return render_template("interests.html")
 
-@app.route('/SoftwareSummary/')
+@app.route('/summary/')
 def summary():
-    return render_template('SoftwareSummary.html')
+    return render_template("summary.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
